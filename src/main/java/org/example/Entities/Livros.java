99e1bffa812479deab810d614ea64d82c0225bb2
Gabriel Misao <gabriel.misao@osf.digital>
2025-02-21 19:18:19 -0300

    Finalizado criação da classe Livros com devidos atributos, métodos e getters and setters

--- src/main/java/org/example/Entities/Livros.java
+++ src/main/java/org/example/Entities/Livros.java
@@ -2,4 +2,75 @@ package org.example.Entities;
 
 public class Livros {
 
+    private int id;
+    private String titulo;
+    private String autor;
+    private int ano;
+    private String genero;
+    private boolean emprestado;
+
+    public void emprestar() {
+        this.emprestado = true;
+    }
+
+    public void devolver(){
+        this.emprestado = false;
+    }
+
+    public Livros(int id, String titulo, String autor, int ano, String genero) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ano = ano;
+        this.genero = genero;
+        this.emprestado = false;
+    }
+
+    public boolean isEmprestado() {
+        return emprestado;
+    }
+
+    public void setEmprestado(boolean emprestado) {
+        this.emprestado = emprestado;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
